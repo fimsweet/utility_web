dd7d6f254477896e85607f892ad92a0a171b9703
hiddcten <xuantruong3204.vietnam@gmail.com>
2025-06-13 07:03:49 +0700

    simple homepage ui

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// src/firebase/config.js
+
+import { initializeApp } from "firebase/app";
+// ... các import khác
+
+// File này an toàn để commit lên GitHub
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+// ... các export khác
